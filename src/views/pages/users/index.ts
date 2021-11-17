8a9d331ff23a4e64ef97da661d8e9cb8cc7ab994
Tomi Markus Alber <tomimarkus.alber@tptlive.ee>
2021-11-17 17:46:27 +0200

    move candidates data

--- src/views/pages/users/index.ts
+++ src/views/pages/users/index.ts
@@ -1,4 +1,3 @@
 export { default as CandidatesPage } from "./CandidatesPage";
 export { default as CandidateDetailsPage } from "./CandidateDetailsPage";
 export { default as CreateCandidatePage } from "./CreateCandidatePage";
-export { default as candidates } from "./candidates.json";
